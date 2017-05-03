5490bed9063ca7bb4d5752fa86d872245d7f5a11
mickboe1 <mickboe23@hotmail.com>
2017-05-03 15:07:54 +0200

    test

---
+++ serialomni3wd/build/CMakeFiles/serialomni3wd.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/serialomni3wd.dir/src/serial.cc.o"
+  "CMakeFiles/serialomni3wd.dir/src/impl/unix.cc.o"
+  "CMakeFiles/serialomni3wd.dir/src/impl/list_ports/list_ports_linux.cc.o"
+  "devel/lib/libserialomni3wd.pdb"
+  "devel/lib/libserialomni3wd.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/serialomni3wd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
